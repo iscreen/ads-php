174f78aae52b2d263b92211f209ee9ff069d349c
Dean Lin <dean.lin@iscreen.com>
2016-02-19 11:46:02 +0800

    Product add retrieve method

--- lib/Product.php
+++ lib/Product.php
@@ -2,7 +2,16 @@
 namespace Ads;
 class Product extends ApiResource
 {
-  
+  /**
+   * @param string $id The ID of the customer to retrieve.
+   * @param array|string|null $opts
+   *
+   * @return Customer
+   */
+  public static function retrieve($id, $opts = null)
+  {
+      return self::_retrieve($id, $opts);
+  }
   /**
    * @param array|null $params
    * @param array|string|null $opts
