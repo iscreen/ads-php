281d67f5bd8385eb6599998c81c5de8bba60fbce
Dean Lin <dean.lin@iscreen.com>
2016-02-19 12:51:08 +0800

    add setApiBase method for Ads class

--- lib/Ads.php
+++ lib/Ads.php
@@ -5,8 +5,7 @@ class Ads
   // @var string The ADS API key to be used for requests.
   public static $apiKey;
   // @var string The base URL for the ADS API.
-  // public static $apiBase = 'https://api.atgames.net';
-  public static $apiBase = 'http://localhost:3000';
+  public static $apiBase = 'https://api.atgames.net';
   // @var string The base URL for the ADS API uploads endpoint.
   public static $apiUploadBase = 'https://uploads.atgames.net';
   // @var string|null The version of the ADS API to use for requests.
@@ -14,8 +13,8 @@ class Ads
   // @var string|null The account ID for connected accounts requests.
   public static $accountId = null;
   // @var boolean Defaults to true.
-  public static $verifySslCerts = false;
-  const VERSION = '0.0.1';
+  public static $verifySslCerts = true;
+  const VERSION = '1.0.1';
   /**
    * @return string The API key used for requests.
    */
@@ -31,6 +30,16 @@ class Ads
   public static function setApiKey($apiKey)
   {
     self::$apiKey = $apiKey;
+  }
+  
+  /**
+   * Sets The base URL for the ADS API.
+   *
+   * @param string $apiBase
+   */
+  public static function setApiBase($apiBase)
+  {
+    self::$apiBase = $apiBase;
   }
   /**
    * @return string The API version used for requests. null if we're using the
